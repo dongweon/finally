375291f4db416109e1ef5683959406eca9d7a6cb
dongweon <961119dw@naver.com>
2022-02-06 21:14:05 +0900

    실수 1개 입력받아 그대로 출력

---
+++ src/codeup12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class codeup12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		float X = sc.nextFloat();
+		System.out.println(X);
+		sc.close();
+
+	}
+
+}
