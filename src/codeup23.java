deaf42f01327527745e1ff26ef1d37574e6aa97c
dongweon <961119dw@naver.com>
2022-02-14 20:22:14 +0900

    실수 1개 입력받아 부분별로 출력

---
+++ src/codeup23.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class codeup23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String[] inputArray = input.split("\\.");
+		System.out.println(inputArray[0] + "\n" + inputArray[1]);
+		sc.close();
+
+	}
+
+}
