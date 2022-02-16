49d790018a9e45bd0dd378f548e7a5f27b9a9c8f
dongweon <961119dw@naver.com>
2022-02-16 16:05:09 +0900

    정수 1개 입력받아 그대로 출력

---
+++ src/codeup/codeup30.java
@@ -0,0 +1,14 @@
+package codeup;
+import java.util.Scanner;
+public class codeup30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long input = sc.nextLong();
+	
+		System.out.println(input);
+		sc.close();
+	}
+
+}
