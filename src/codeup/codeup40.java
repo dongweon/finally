270000d4fb62b084da1df7aa12ab60929515e7fd
dongweon <961119dw@naver.com>
2022-02-16 17:46:28 +0900

    정수 1개 입력받아 부호 바꿔 출력

---
+++ src/codeup/codeup40.java
@@ -0,0 +1,15 @@
+package codeup;
+import java.util.Scanner;
+public class codeup40 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		System.out.println((-1)*input);
+		sc.close();
+
+	}
+
+}
