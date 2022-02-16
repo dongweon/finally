79e66d6110da76365ed13a327698648bf8e317f7
dongweon <961119dw@naver.com>
2022-02-16 17:58:01 +0900

    문자 1개 입력받아 다음 문자 출력

---
+++ src/codeup/codeup41.java
@@ -0,0 +1,14 @@
+package codeup;
+import java.util.Scanner;
+public class codeup41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char input = sc.next().charAt(0);
+		char output = (char)(input + 1);
+		System.out.println(output);
+		sc.close();
+	}
+
+}
