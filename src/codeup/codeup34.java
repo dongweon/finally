b7570785f663aa4dfa49638527951269bc3ed7c9
dongweon <961119dw@naver.com>
2022-02-16 17:15:04 +0900

    8진 정수 1개 입력받아 10진수로 출력

---
+++ src/codeup/codeup34.java
@@ -0,0 +1,16 @@
+package codeup;
+import java.util.Scanner;
+public class codeup34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		int input8 = Integer.parseInt(input, 8);
+		
+		System.out.println(input8);
+		sc.close();
+
+	}
+
+}
