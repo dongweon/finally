0b4d7525ce472d78b6b8a14f736b8c71811673fe
dongweon <961119dw@naver.com>
2022-02-16 17:36:15 +0900

    정수 2개 입력받아 합 출력

---
+++ src/codeup/codeup38.java
@@ -0,0 +1,16 @@
+package codeup;
+import java.util.Scanner;
+public class codeup38 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long input1 = sc.nextLong();
+		long input2 = sc.nextLong();
+		
+		System.out.println(input1 + input2);
+		sc.close();
+
+	}
+
+}
