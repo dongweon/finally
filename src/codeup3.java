97f9e76d434f6781d83c7b7b5ac0dac316fdb908
dongweon <961119dw@naver.com>
2022-02-16 14:49:55 +0900

    년월일 입력 받아 형식 바꿔 출력

--- src/codeup3.java
+++ src/codeup3.java
@@ -4,7 +4,7 @@ public class codeup3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello" + "\n" + "World");
-		System.out.println("Hello\nWorld");
+		//System.out.println("Hello\nWorld");
 	}
 
 }
