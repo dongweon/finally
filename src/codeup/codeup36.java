389200d970e9926621abfc8c467a773a6bb8bc35
dongweon <961119dw@naver.com>
2022-02-16 17:27:59 +0900

    영문자 1개 입력받아 10진수로 출력

---
+++ src/codeup/codeup36.java
@@ -0,0 +1,14 @@
+package codeup;
+import java.util.Scanner;
+public class codeup36 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char input = sc.next().charAt(0);
+		
+		System.out.println((int)input);		
+		sc.close();
+	}
+
+}
