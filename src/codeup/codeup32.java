c3a6dd88669a579a637d21b85416ec89d0448d1d
dongweon <961119dw@naver.com>
2022-02-16 16:23:27 +0900

    10진 정수 입력받아 16진수로 출력

---
+++ src/codeup/codeup32.java
@@ -0,0 +1,15 @@
+package codeup;
+import java.util.Scanner;
+public class codeup32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		System.out.format("%x", input);
+		sc.close();
+
+	}
+
+}
