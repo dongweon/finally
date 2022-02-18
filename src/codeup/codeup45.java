0caf743dbcaecb768db0899ecab8f003e858a20a
dongweon <961119dw@naver.com>
2022-02-18 15:20:34 +0900

    정수 2개 입력받아 자동 계산

---
+++ src/codeup/codeup45.java
@@ -0,0 +1,22 @@
+package codeup;
+import java.util.Scanner;
+public class codeup45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String[] arr = input.split(" ");
+		int a = Integer.parseInt(arr[0]);
+		int b = Integer.parseInt(arr[1]);
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.format("%.2f", (float)a/b);
+		sc.close();
+	}
+
+}
