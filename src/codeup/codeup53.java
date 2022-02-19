7309c10ab2ec0dc3e7dd63269b6efd84ec8d6859
dongweon <961119dw@naver.com>
2022-02-19 15:08:47 +0900

    참 거짓 바꾸기

---
+++ src/codeup/codeup53.java
@@ -0,0 +1,14 @@
+package codeup;
+import java.util.Scanner;
+public class codeup53 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		System.out.println(input==0 ? 1:0);
+		sc.close();
+	}
+
+}
