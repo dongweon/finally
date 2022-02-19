4e4b89ef865fc30731bbcb3dd42ecc64f7dfa961
dongweon <961119dw@naver.com>
2022-02-19 15:14:44 +0900

    둘 다 참일 경우만 참 출력

---
+++ src/codeup/codeup54.java
@@ -0,0 +1,18 @@
+package codeup;
+import java.util.Scanner;
+public class codeup54 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String[] arr = input.split(" ");
+		int a = Integer.parseInt(arr[0]);
+		int b = Integer.parseInt(arr[1]);
+		
+		System.out.println(a==1 && b==1 ? 1:0);
+		sc.close();
+
+	}
+
+}
