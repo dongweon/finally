a2428d9f7f2062fc0e754882292731529db69d68
dongweon <961119dw@naver.com>
2022-02-20 10:50:56 +0900

    비트단위로 AND/OR/XOR하여 출력

---
+++ src/codeup/codeup60.java
@@ -0,0 +1,20 @@
+package codeup;
+import java.util.Scanner;
+public class codeup60 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String[] arr = input.split(" ");
+		int a = Integer.parseInt(arr[0]);
+		int b = Integer.parseInt(arr[1]);
+		
+		System.out.println(a & b);
+		// or |
+		//xor ^
+		sc.close();
+
+	}
+
+}
