8c713288dac138df010c16f8788f0f51c6c86bb1
dongweon <961119dw@naver.com>
2022-02-22 07:37:52 +0900

    0 입력될 때까지 무한 출력

---
+++ src/codeup/codeup71.java
@@ -0,0 +1,22 @@
+package codeup;
+import java.util.Scanner;
+public class codeup71 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while(true) {
+		int input = sc.nextInt();
+			if (input != 0)
+				System.out.println(input);
+			else
+				break;
+		}
+		
+		sc.close();
+
+	}
+
+}
