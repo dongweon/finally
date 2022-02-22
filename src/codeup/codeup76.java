2ab69c3c26c069397c7c34c730db691dda31f6f7
dongweon <961119dw@naver.com>
2022-02-22 12:12:13 +0900

    문자 1개 입력받아 알파벳 출력

---
+++ src/codeup/codeup76.java
@@ -0,0 +1,18 @@
+package codeup;
+import java.util.Scanner;
+public class codeup76 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = 'a'; //97
+		char input = sc.next().charAt(0);
+		
+		for(int i=a; i<=(int)input; i++) {
+			System.out.println((char)i);
+		}
+		sc.close();
+
+	}
+
+}
