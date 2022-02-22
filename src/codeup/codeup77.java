85b994c4108db1d8983ced7d3819aef0f1020068
dongweon <961119dw@naver.com>
2022-02-22 12:28:21 +0900

    정수 1개 입력받아 그 수까지 출력

---
+++ src/codeup/codeup77.java
@@ -0,0 +1,16 @@
+package codeup;
+import java.util.Scanner;
+public class codeup77 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		for(int i=0; i<=input; i++)
+			System.out.println(i);
+		sc.close();
+
+	}
+
+}
