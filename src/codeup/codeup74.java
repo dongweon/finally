d487f9be400b157f3b76b21e06cc8ba2e5646fd6
dongweon <961119dw@naver.com>
2022-02-22 11:25:58 +0900

    정수 1개 입력받아 카운트다운 출력

---
+++ src/codeup/codeup74.java
@@ -0,0 +1,19 @@
+package codeup;
+import java.util.Scanner;
+public class codeup74 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		while (input > 0) {
+			System.out.println(input--);
+		}
+		// for(int i=0; i<input; i++)
+		//	System.out.println(input-i);
+		sc.close();
+
+	}
+
+}
