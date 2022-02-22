254c581bc7763bce05f8d62ba434d1e540521c07
dongweon <961119dw@naver.com>
2022-02-22 14:01:12 +0900

    원하는 문자 입력될 때까지 반복 출력

---
+++ src/codeup/codeup79.java
@@ -0,0 +1,21 @@
+package codeup;
+import java.util.Scanner;
+public class codeup79 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			String input = sc.next();
+			if (input.equals("q")) {
+				System.out.println(input);
+				break;
+			}
+				System.out.println(input);
+			}
+		sc.close();
+
+	}
+
+}
