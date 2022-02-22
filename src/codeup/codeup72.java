d0bd1901edfabe93a2fc6779a79d78a726275939
dongweon <961119dw@naver.com>
2022-02-22 10:59:47 +0900

    정수 입력받아 계속 출력

---
+++ src/codeup/codeup72.java
@@ -0,0 +1,18 @@
+package codeup;
+import java.util.Scanner;
+public class codeup72 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		for(int i=0; i<input; i++) {
+			int answer = sc.nextInt();
+			System.out.println(answer);
+		}
+		sc.close();
+
+	}
+
+}
