0dc3ea64e2afc3da0d249ff760654291202fa8e7
dongweon <961119dw@naver.com>
2022-02-23 16:34:12 +0900

    여기까지! 이제 그만~

---
+++ src/codeup/codeup87.java
@@ -0,0 +1,23 @@
+package codeup;
+import java.util.Scanner;
+public class codeup87 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int sum = 0;
+		
+		for(int i=0; i<input; i++) {
+			sum += i;
+			if (sum >= input) {
+				System.out.println(sum);
+			}
+				break;
+			
+		}
+		sc.close();
+
+	}
+
+}
