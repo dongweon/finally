9dee8199e6b08179c34bef8191f63538b60934fd
dongweon <961119dw@naver.com>
2022-02-24 07:00:50 +0900

    수 나열하기1

---
+++ src/codeup/codeup89.java
@@ -0,0 +1,19 @@
+package codeup;
+import java.util.Scanner;
+public class codeup89 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int d = sc.nextInt();
+		int n = sc.nextInt();
+		int result = a + d*(n-1);
+		
+		System.out.println(result);
+		
+		sc.close();
+
+	}
+
+}
