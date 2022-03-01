e9fb9b58be77a3ed2623eec25f55d3f4d528d048
dongweon <961119dw@naver.com>
2022-03-01 11:05:52 +0900

    ~ 알람 시계

---
+++ src/baek/baek_2884.java
@@ -0,0 +1,25 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek_2884 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if(h == 0) 
+			System.out.println((h+23) + " " + (m+15));
+		else {
+			if(m < 45)
+				System.out.println(h-1 + " " + (m+15));
+			else
+				System.out.println(h + " " + (m-45));
+		}
+		sc.close();
+
+	}
+
+}
