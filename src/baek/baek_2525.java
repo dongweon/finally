6a131e8a93905f07b73c084c6c062ad428c85371
dongweon <961119dw@naver.com>
2022-03-01 11:32:32 +0900

    오븐 시계

---
+++ src/baek/baek_2525.java
@@ -0,0 +1,30 @@
+package baek;
+import java.util.Scanner;
+public class baek_2525 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int hour = sc.nextInt();
+		int minute = sc.nextInt();
+		int time = sc.nextInt();
+		
+		hour += time / 60;
+		minute += time % 60;
+		
+		if(minute >= 60) {
+			hour += 1;
+			minute -= 60;
+		}
+		
+		if(hour >= 24)
+			hour -= 24;
+		
+		System.out.println(hour + " " + minute);
+
+		
+		sc.close();
+
+	}
+
+}
