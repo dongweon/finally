0d0d561f28f39d5f9d190f74960ec04d507f8e6c
dongweon <961119dw@naver.com>
2022-03-03 07:51:47 +0900

    별 찍기-1

---
+++ src/baek/baek_.java
@@ -0,0 +1,23 @@
+package baek;
+
+import java.util.Scanner;
+
+public class baek_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for(int i=0; i<num; i++) { 
+			for(int j=0; j<i+1; j++) { 
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+
+	}
+
+}
