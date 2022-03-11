6e087e70c67e2a9e9c7884a8bfc60083bcb0a68d
dongweon <961119dw@naver.com>
2022-03-12 06:56:25 +0900

    아스키 코드

---
+++ Baek/String/baek_11654.java
@@ -0,0 +1,16 @@
+package String;
+import java.util.Scanner;
+public class baek_11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.next().charAt(0);
+		
+		System.out.println(input);
+		
+		sc.close();
+
+	}
+
+}
