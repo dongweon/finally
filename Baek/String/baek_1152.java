9f646c723698978c61c302e58861e5791cd69ebf
dongweon <961119dw@naver.com>
2022-03-15 07:26:01 +0900

    단어의 개수

---
+++ Baek/String/baek_1152.java
@@ -0,0 +1,21 @@
+package String;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class baek_1152 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input = sc.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(input, " ");
+		// 공백을 기준으로 나눈 토큰들을 st에 저장
+		
+		System.out.println(st.countTokens());
+		// countTokens() : 토큰의 개수 반환
+		
+		sc.close();
+
+	}
+
+}
