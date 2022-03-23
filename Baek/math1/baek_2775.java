c44f6f9e212e2308df9973f8a1db16cc936e9582
dongweon <961119dw@naver.com>
2022-03-23 09:57:44 +0900

    부녀회장이 될테야

--- Baek/math1/baek_2775.java
+++ Baek/math1/baek_2775.java
@@ -1,33 +1,45 @@
 package math1;
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class baek_2775 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static int[][] APT = new int[15][15];
+	
+	public static void main(String[] args) throws IOException {
 		
-		int[][] apt = new int[15][15];
-			
-		for(int i = 0; i < 15; i++) {
-			apt[i][1] = 1;
-			apt[0][i] = i;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		make_APT();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < T; i++) {
+			int k = Integer.parseInt(br.readLine());
+			int n = Integer.parseInt(br.readLine());
+			sb.append(APT[k][n]).append('\n');
 		}
 		
-		for(int i = 1; i < 15 ; i++) {
-			for(int j = 2; j < 14; j++) {
-				apt[i][j] = apt[i][j-1] + apt[i - 1][j];
+		System.out.println(sb);
+
+	}
+	
+	public static void make_APT() {
+		
+		for(int i = 0; i < 15; i++) {
+			APT[i][1] = 1;
+			APT[0][i] = i;
+		}
+		
+		for(int i = 1; i < 15; i++) {
+			for(int j = 2; j < 15; j++) {
+				APT[i][j] = APT[i][j-1] + APT[i-1][j];
 			}
 		}
 		
-		int T = sc.nextInt();
-		
-		for(int i = 0; i < T; i++) {
-			int k = sc.nextInt();
-			int n = sc.nextInt();
-			
-			System.out.println(apt[k][n]);
-		}
-		sc.close();
-
 	}
 
 }
