c07bcf1564afd6cd2c118cc93d59569695b2295c
dongweon <961119dw@naver.com>
2022-04-26 21:13:11 +0900

    일탈 끝

---
+++ Programmers/level1/even_odd.java
@@ -0,0 +1,17 @@
+package level1;
+
+public class even_odd {
+
+	public String solution(int num) {
+		
+		String answer = "";
+		
+		if(num % 2 == 0 ) {
+			return "Even";
+		}
+		else
+			return "Odd";
+
+	}
+
+}
