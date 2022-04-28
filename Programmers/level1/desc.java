49dafe75f56ec6610bcd9535ae32501496c9ed78
dongweon <961119dw@naver.com>
2022-04-28 16:23:29 +0900

    정수 내림차순으로 배치하기

--- Programmers/level1/desc.java
+++ Programmers/level1/desc.java
@@ -1,10 +1,29 @@
 package level1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class desc {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public long solution(long n) {
+		
+		ArrayList<Long> arr = new ArrayList<>();
+		String answer = "";
+		
+		while(n != 0) {
+			arr.add(n % 10);
+			n /= 10;
+		}
+		
+		// Collections.reverseOrder() : 내림차순으로 바꿔줌
+		Collections.sort(arr,Collections.reverseOrder());
+		
+		// ArrayList의 원소들을 문자열에 붙여줌
+		for(long x : arr) {
+			answer += String.valueOf(x);
+		}
+		
+		return Long.parseLong(answer);
 	}
 
 }
