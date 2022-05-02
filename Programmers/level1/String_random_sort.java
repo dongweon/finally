32093a7a44d075ff7ceb428dc6c9b1653b2ca77b
dongweon <961119dw@naver.com>
2022-05-02 11:30:19 +0900

    문자열 내 마음대로 정렬하기

---
+++ Programmers/level1/String_random_sort.java
@@ -0,0 +1,28 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class String_random_sort {
+
+	public String[] solution(String[] strings, int n) {
+		
+		String[] answer = {};
+		ArrayList<String> arr = new ArrayList<>();
+		
+		for(int i = 0; i < strings.length; i++) {
+			arr.add("" + strings[i].charAt(n) + strings[i]);
+		}
+		
+		Collections.sort(arr);
+		answer = new String[arr.size()];
+		
+		for(int i = 0; i < arr.size(); i++) {
+			answer[i] = arr.get(i).substring(1);
+		}
+		
+		return answer;
+
+	}
+
+}
